fc4e0d7383064eef489b37777d3e9db6c8182e75
Jakub Chojna <jakub.chojna@gmail.com>
2024-04-14 21:39:06 +0200

    feat: install glob3d

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,38 @@
 // @ts-expect-error ignore missing glob3d types
 import { BarGlob3d } from 'glob3d';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import './App.css';
-
 import { getCitiesData } from './citiesData';
-
-// const root = document.querySelector('#root');
-// const updateButton = document.querySelector('.update-button');
-// if (!root || !(root instanceof HTMLElement)) {
-//   throw new Error('Root element not found');
-// }
-
-// updateButton &&
-//   updateButton.addEventListener('click', async () => {
-//     globeInstance.clean();
-//     globeInstance.update(data);
-//   });
 
 function App() {
   const appRef = useRef<HTMLDivElement | null>(null);
-
   const data = getCitiesData();
+  const [globeInstance, setGlobeInstance] = useState<BarGlob3d | null>(null);
 
   useEffect(() => {
     if (!appRef.current) return;
     if (appRef.current.children.length === 0) {
-      const globeInstance = new BarGlob3d(appRef.current, {
-        debugMode: false,
-        tooltipValueSuffix: 'people',
-      });
-      globeInstance.initialize(data);
+      setGlobeInstance(
+        new BarGlob3d(appRef.current, data, {
+          debugMode: false,
+          tooltipValueSuffix: 'people',
+        })
+      );
     }
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-  return <div ref={appRef} style={{ width: '1200px', height: '800px' }}></div>;
+  return (
+    <>
+      <div ref={appRef} style={{ width: '1200px', height: '800px' }}></div>
+      <button
+        onClick={() => globeInstance.update(data)}
+        className="update-button"
+      >
+        Update
+      </button>
+    </>
+  );
 }
 
 export default App;
